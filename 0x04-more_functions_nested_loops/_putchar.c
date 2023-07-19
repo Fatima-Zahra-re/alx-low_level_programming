e26fc627d962377bcbeabb3676262ae317d46dfb
fatima zahra <fatima.zahraredd@gmail.com>
2023-07-19 06:46:15 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include "main.h"
+/**
+ * main - prints string from a character array.
+ * @c : the character to print
+ * Return: 1 on success.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
